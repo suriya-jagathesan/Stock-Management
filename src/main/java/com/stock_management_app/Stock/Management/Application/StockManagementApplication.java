1795fbb458e033f0aa833646cd2d9550b12fda5b
chakkaravarthyJs <surya@bizappln.com>
2025-11-24 22:01:00 +0530

    Initial commit

---
+++ src/main/java/com/stock_management_app/Stock/Management/Application/StockManagementApplication.java
@@ -0,0 +1,14 @@
+package com.stock_management_app.Stock.Management.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StockManagementApplication.class, args);
+	}
+
+}
